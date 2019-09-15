94f5fc784418e2ea3396e92dd2157cffe1f3052e
ShadmanAadeeb <aadeeb1896@gmail.com>
2019-09-15 21:21:11 +0600

    Initial commit

---
+++ app/src/main/java/com/example/githubproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.githubproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //This is the first comment
+    }
+}
